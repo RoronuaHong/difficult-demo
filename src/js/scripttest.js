6550ed076c371564370151b115078d8a6551cb98
SlimHong <807672412@qq.com>
2018-03-06 17:09:13 +0800

    重新封装script加载事件

--- src/js/scripttest.js
+++ src/js/scripttest.js
@@ -1,3 +1,42 @@
+/**
+ * Created by SlimHong on 2018/0306
+ */
+//判断JS是否加载完成
+//我们使用document的readyState属性: document.readyState
+//readyState 属性返回当前文档的状态:
+//
+//uninitialized - 还未开始加载
+//loading - 加载中
+//interactive - 已加载, 文档与用户可以开始交互
+//complete - 载入完成(loaded)
+
+//封装loadScript
+function loadScript(url, callback) {
+    var script = document.createElement("script");
+
+    script.type = "text/javascript";
+    script.src = url;
+    script.async = "async";
+
+    document.appendChild(script);
+
+    if(script.readyState) {     //IE
+        script.onreadystatechange = function() {
+            if(script.readyState === "completed" || script.readyState === "loaded") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        }
+    } else {        //非IE
+        script.onload = function() { callback() }
+    }
+
+    //绑定资源加载失败事件
+    script.onerror = function() {
+
+    }
+}
+
 var script = document.createElement("script");
 var head = document.getElementsByTagName("head")[0];
 
